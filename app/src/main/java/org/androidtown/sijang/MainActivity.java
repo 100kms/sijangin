aa42249e65520a69b23f528df95e69671025a97d
KimMinHyuk8375 <mh1203@daum.net>
2017-08-11 18:41:29 +0900

    v0.12

--- app/src/main/java/org/androidtown/sijang/MainActivity.java
+++ app/src/main/java/org/androidtown/sijang/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ReviewList.class);
+                Intent intent = new Intent(getApplicationContext(), FoodMenuList.class);
                 startActivity(intent);
             }
         });
