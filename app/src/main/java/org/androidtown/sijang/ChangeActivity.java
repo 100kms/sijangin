4ca244057a9e466c76b60f6522a2e777c08ea4f1
closeweb <dksguddn@nate.com>
2017-08-18 15:14:44 +0900

    v0.20 송정은 push_test

--- app/src/main/java/org/androidtown/sijang/ChangeActivity.java
+++ app/src/main/java/org/androidtown/sijang/ChangeActivity.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 
 public class ChangeActivity extends AppCompatActivity {
 
-    //aabb_prettyje.
+    //aabb_prettyje...
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
