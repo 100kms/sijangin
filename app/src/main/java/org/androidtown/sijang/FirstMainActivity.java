fc278c59a94c9ce25ef3e4238d336453c673e75a
closeweb <songje99@naver.com>
2017-08-19 19:19:35 +0900

    v0.23 송정은

--- app/src/main/java/org/androidtown/sijang/FirstMainActivity.java
+++ app/src/main/java/org/androidtown/sijang/FirstMainActivity.java
@@ -1,13 +1,30 @@
 package org.androidtown.sijang;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class FirstMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_first_main);
+
+        Button change_btn = (Button)findViewById(R.id.button_chagne);
+        change_btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
